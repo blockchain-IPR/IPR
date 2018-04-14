bd8e5bc9eac1f674cb2df67f8769410443c486dc
zhaolei@easyact.cn <zhaolei@easyact.cn>
2018-04-14 12:13:29 +0800

    +api init

---
+++ contracts/IPR.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.17;
+
+contract IPR {
+    struct Project {
+        string name;
+        string desc;
+        uint32 timestamp;
+    }
+
+    Project[16] public projects;
+    address[16] public users;
+    // 分支
+    function branch(uint projectId) public returns (uint) {
+        require(projectId >= 0 && projectId <= 15);
+        users[projectId] = msg.sender;
+        return projectId;}
+    // 返回用户
+    function getUsers() public view returns (address[16]) {
+        return users;
+    }
+    // 返回项目列表
+    function getProjects() public view returns (address[16]) {
+        return projects;
+    }
+}
