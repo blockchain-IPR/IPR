0f2c83e62853fe9a835ac727c6d127e589054c67
zhaolei@easyact.cn <zhaolei@easyact.cn>
2018-04-14 11:32:13 +0800

    +web3通信

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,72 @@
 App = {
-  web3Provider: null,
-  contracts: {},
+    web3Provider: null,
+    contracts: {},
 
-  init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    init: function () {
+        // Load pets.
+        $.getJSON('../pets.json', function (data) {
+            const petsRow = $('#petsRow')
+            const petTemplate = $('#petTemplate')
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+            for (i = 0; i < data.length; i++) {
+                petTemplate.find('.panel-title').text(data[i].name)
+                petTemplate.find('img').attr('src', data[i].picture)
+                petTemplate.find('.pet-breed').text(data[i].breed)
+                petTemplate.find('.pet-age').text(data[i].age)
+                petTemplate.find('.pet-location').text(data[i].location)
+                petTemplate.find('.btn-adopt').attr('data-id', data[i].id)
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+                petsRow.append(petTemplate.html())
+            }
+        })
 
-    return App.initWeb3();
-  },
+        return App.initWeb3()
+    },
 
-  initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    initWeb3: function () {
+// Is there an injected web3 instance?
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+        } else {
+// If no injected web3 instance is detected, fall back to Ganache
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+        }
+        web3 = new Web3(App.web3Provider)
+        return App.initContract()
+    },
 
-    return App.initContract();
-  },
+    initContract: function () {
+        /*
+         * Replace me...
+         */
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+        return App.bindEvents()
+    },
 
-    return App.bindEvents();
-  },
+    bindEvents: function () {
+        $(document).on('click', '.btn-adopt', App.handleAdopt)
+    },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+    markAdopted: function (adopters, account) {
+        /*
+         * Replace me...
+         */
+    },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+    handleAdopt: function (event) {
+        event.preventDefault()
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+        const petId = parseInt($(event.target).data('id'))
 
-    var petId = parseInt($(event.target).data('id'));
+        /*
+         * Replace me...
+         */
+    }
 
-    /*
-     * Replace me...
-     */
-  }
+}
 
-};
-
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
-});
+$(function () {
+    $(window).load(function () {
+        App.init()
+    })
+})
