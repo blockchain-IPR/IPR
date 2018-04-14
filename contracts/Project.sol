6ce4626046e89eb043f437d715e2e4f2158c7135
zhaolei@easyact.cn <zhaolei@easyact.cn>
2018-04-14 16:13:04 +0800

    +Project

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,6 +1,20 @@
 pragma solidity ^0.4.17;
 
 contract Project {
+    uint projectId;
     string public name;
     string public desc;
+    address owner;
+    uint timestamp;
+    uint parentId;
+
+    function Project(uint _parentId, string _name, string _desc) public {
+        require(bytes(_name).length > 2);
+        timestamp = now;
+        projectId = uint(keccak256(timestamp));
+        parentId = _parentId;
+        name = _name;
+        desc = _desc;
+        owner = msg.sender;
+    }
 }
